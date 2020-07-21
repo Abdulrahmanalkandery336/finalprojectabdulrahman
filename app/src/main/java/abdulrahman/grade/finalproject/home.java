23333f9f330b97f7b6db9745634261bc63fbf2de
Abdulrahmanalkandery336 <hamanialk123@gmail.com>
2020-07-21 10:48:45 +0300

    Initial commit

---
+++ app/src/main/java/abdulrahman/grade/finalproject/home.java
@@ -0,0 +1,30 @@
+package abdulrahman.grade.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button alb = findViewById(R.id.button13);
+        alb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent hell =new Intent(home.this,it.class);
+                startActivity(hell);
+            }
+        });
+
+
+
+
+    }
+}
---
+++ app/src/main/java/abdulrahman/grade/finalproject/it.java
@@ -0,0 +1,14 @@
+package abdulrahman.grade.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class it extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_it);
+    }
+}
